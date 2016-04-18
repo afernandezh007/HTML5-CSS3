2b80f2ffdb9d2e7b8aa9769b2e41f5396fe60ba6
Antonio Fernández <antonio.fernandez.hernandez@gmail.com>
2016-04-18 19:54:33 +0200

    Se incluye bower. Uso de animaciones en css

--- code/Gruntfile.js
+++ code/Gruntfile.js
@@ -1,53 +1,65 @@
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    pkg: grunt.file.readJSON('package.json'),
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
 
-    less: {
-      development: {
-          files: {
-              "style.css": "less/style.less"
-          }
-      },
-      production: {
-          options: {
-            compress: true
-          },
-          files: {
-              "style.css": "less/style.less"
-          }
-      }
-    },
-
-    browserSync: {
-        dev: {
-            bsFiles: {
-                src: ['*.css', '*.html']
+        less: {
+            development: {
+                files: {
+                    "style.css": "less/style.less"
+                }
             },
-            options: {
-                watchTask: true,
-                server: './'
+            production: {
+                options: {
+                    compress: true
+                },
+                files: {
+                    "style.css": "less/style.less"
+                }
+            }
+        },
+
+        browserSync: {
+            dev: {
+                bsFiles: {
+                    src: ['*.css', '*.html']
+                },
+                options: {
+                    watchTask: true,
+                    server: './'
+                }
+            }
+        },
+
+        imagemin: {
+            dynamic: {
+                files: [{
+                    expand: true,
+                    src: ['img/*.{png,jpg,gif}'], // Actual patterns to match
+                    dest: 'dist/' // Destination path prefix
+                }]
+            }
+        },
+
+        watch: {
+            styles: {
+                files: ['less/*.less'],
+                tasks: ['less:development']
             }
         }
-    },
 
-    watch: {
-      styles: {
-        files: ['less/*.less'],
-        tasks: ['less:development']
-      }
-    }
+    });
 
-  });
+    // Loads the plugins
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-browser-sync');
+    grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  // Loads the plugins
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-browser-sync');
-
-  // Default task(s).
-  grunt.registerTask('default', ['less:development', 'browserSync', 'watch']);
-  grunt.registerTask('production', ['less:production']);
+    // Default task(s).
+    grunt.registerTask('default', ['less:development', 'browserSync', 'watch']);
+    grunt.registerTask('production', ['less:production']);
+    grunt.registerTask('images', ['imagemin']);
 
 };
